c3a1dd3ffe21f1a0e45b98a5b810fc0a89240d32
LeTruePandamonium <kurtallenalfonso@gmail.com>
2025-02-14 12:54:34 +0800

    Initial commit

---
+++ 02TPProgrammingLanguages.py
@@ -0,0 +1,39 @@
+#number 1#
+
+print ("Daitanfuteki ni haikara kakumei")
+print ("Rairairakuraku hansen kokka")
+
+#number 2#
+
+print("Daitanfuteki ni haikara kakumei")
+print()
+print("Rairairakuraku hansen kokka")
+
+#number 3#
+
+print("Daitanfuteki ni haikara kakumei", "\nRairairakuraku hansen kokka")
+
+#number 4#
+
+print("\nDaitanfuteki", "ni haikara", "kakumei")
+print()
+print("\nRairairakuraku hansen kokka")
+
+#number 5#
+
+print("\nDaitanfuteki ni haikara kakumei", end=" ")
+print()
+print("\nRairairakuraku hansen kokka")
+
+#number 6#
+
+print ("Daitanfuteki", "ni haikara", "kakumei", end=" ")
+print()
+print("Rairairakuraku", "hansen", "kokka", sep="---")
+
+#number 7#
+
+print("Daitanfuteki ni", "haikara", "kakumei", sep="_", end=" ")
+print()
+print("Rairairakuraku", "hansen", "kokka", sep="--")
+
